2a5be1c48eacb15267b0d235e21801b5528ceeb3
Saksham agarwal <45118162+Dragonsa15@users.noreply.github.com>
2022-05-09 23:51:26 +0530

    Adding entrypoint for ansible

---
+++ .github/actions/ansible/entrypoint.sh
@@ -0,0 +1,10 @@
+echo "Ansible Entrypoint"
+
+# echo "This is the secret: $SSH_PASSWORD"
+
+echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
+echo "ansible_pass=$SSH_PASSWORD" >> /hosts
+echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
+echo "ansible_sudo_pass=$SSH_PASSWORD" >> /hosts
+
+ansible-playbook ansible/deploy.yml --user saksham
