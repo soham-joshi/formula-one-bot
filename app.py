511c0b931277dabe4933e401ffd11a34a367786c
Saksham Agarwal <saksham.aladin@gmail.com>
2022-05-09 22:47:08 +0530

    adding initial logging command in bot

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ import os
 import api.config
 from api import commands
     
+logging.basicConfig(level=logging.NOTSET,
+                filename="Calclogs.log",filemode="a",
+                format="%(asctime)s %(levelname)s-%(message)s",
+                datefmt='%Y-%m-%d %H:%M:%S')
+                
 commands.bot.run(api.config.CONFIG['BOT']['TOKEN'])
